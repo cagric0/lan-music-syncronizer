b81c53c3510e4e454518bf22eb1e9e2041e2eba1
Muhammed HALAS <muhammed.halas@obss.com.tr>
2021-02-08 00:29:24 +0300

    UI done

--- globals.py
+++ globals.py
@@ -1,4 +1,7 @@
 COLORS = ["#edffec", "#61e786", "#5a5766", "#48435c", "#9792e3"]
 LARGEFONT = ("Helvetica", 32)
-MEDIUMFONT = ("Helvetica", 24)
-SMALLFONT = ("Helvetica", 16)
+MEDIUMFONT = ("Helvetica", 16)
+SMALLFONT = ("Helvetica", 12)
+MUSIC_LIBRARY_PATH = "music/"
+created_room_ip = ""
+ourIP = "cagri"
